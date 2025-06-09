872aadf947b4772ee237129feb83b55422277eb3
Jagggbet <betjaguar99@gmail.com>
2025-06-09 22:18:03 +0400

    Depoyu temizliyorum (tüm dosyaları sildim)

--- functions/api/auth/nonce.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet(context) {
-    const nonce = crypto.randomUUID();
-    return new Response(JSON.stringify({ nonce }), { headers: { 'Content-Type': 'application/json' } });
-}
-
